fe798e2cccbf15891538f1e931427df8e3b25a57
Quinn McHenry <quinn@jqm.us>
2017-08-09 16:21:14 -0400

    Add buttons to main UI

--- smallplanet_Pinball/main/MainController.swift
+++ smallplanet_Pinball/main/MainController.swift
@@ -10,22 +10,42 @@ import UIKit
 import PlanetSwift
 import Laba
 
-
 class MainController: PlanetViewController {
+
+    func sendButtonPress(left: Bool) {
+        print("Coming soon to a pinball machine near you...")
+    }
+    
+    func leftButtonPress() {
+        sendButtonPress(left: true)
+    }
+    
+    func rightButtonPress() {
+        sendButtonPress(left: false)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         mainBundlePath = "bundle://Assets/main/main.xml"
         loadView()
         
         titleLabel.view.Animate("!<100!f")
+        
+        leftButton.button.addTarget(self, action: #selector(MainController.leftButtonPress), for: .touchDown)
+        rightButton.button.addTarget(self, action: #selector(MainController.rightButtonPress), for: .touchDown)
+
     }
 
     fileprivate var titleLabel: View {
         return mainXmlView!.elementForId("titleLabel")!.asView!
     }
-    
+    fileprivate var leftButton: Button {
+        return mainXmlView!.elementForId("leftButton")!.asButton!
+    }
+    fileprivate var rightButton: Button {
+        return mainXmlView!.elementForId("rightButton")!.asButton!
+    }
+
 }
 
