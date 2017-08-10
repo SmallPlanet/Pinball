d0def4120fd67582da90f8bfe227ed97a00aa94a
Rocco Bowling <rocco@chimerasw.com>
2017-08-10 12:31:25 -0400

    moved all test control code to its own view controller

--- smallplanet_Pinball/control/ControlController.swift
+++ smallplanet_Pinball/control/ControlController.swift
@@ -9,17 +9,60 @@
 import UIKit
 import PlanetSwift
 import Laba
-
+import SwiftSocket
 
 class ControlController: PlanetViewController {
+    
+    var client: TCPClient!
+    
+    func sendButtonPress(left: Bool) {
+        let result = client.send(string: left ? "LEFT" : "RIGHT")
+        print(result)
+    }
+    
+    func leftButtonPress() {
+        sendButtonPress(left: true)
+    }
+    
+    func rightButtonPress() {
+        sendButtonPress(left: false)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Control Mode"
         
+        client = TCPClient(address: "192.168.3.1", port: 8000)
+        
         mainBundlePath = "bundle://Assets/control/control.xml"
         loadView()
+        
+        leftButton.button.addTarget(self, action: #selector(ControlController.leftButtonPress), for: .touchDown)
+        rightButton.button.addTarget(self, action: #selector(ControlController.rightButtonPress), for: .touchDown)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        switch client.connect(timeout: 3) {
+        case .success:
+            print("Connection successful 🎉")
+        case .failure(let error):
+            print("Connectioned failed 💩")
+            print(error)
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        client.close()
+    }
+    
+    fileprivate var leftButton: Button {
+        return mainXmlView!.elementForId("leftButton")!.asButton!
+    }
+    fileprivate var rightButton: Button {
+        return mainXmlView!.elementForId("rightButton")!.asButton!
     }
     
 }
--- smallplanet_Pinball/main/MainController.swift
+++ smallplanet_Pinball/main/MainController.swift
@@ -9,32 +9,14 @@
 import UIKit
 import PlanetSwift
 import Laba
-import SwiftSocket
 
 class MainController: PlanetViewController {
-
-    var client: TCPClient!
-    
-    func sendButtonPress(left: Bool) {
-        let result = client.send(string: left ? "LEFT" : "RIGHT")
-        print(result)
-    }
-    
-    func leftButtonPress() {
-        sendButtonPress(left: true)
-    }
-    
-    func rightButtonPress() {
-        sendButtonPress(left: false)
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "Pinball"
 
-        client = TCPClient(address: "192.168.3.1", port: 8000)
-        
         mainBundlePath = "bundle://Assets/main/main.xml"
         loadView()
 		
@@ -45,39 +27,13 @@ class MainController: PlanetViewController {
         controlModeButton.button.add(for: .touchUpInside) {
             self.navigationController?.pushViewController(ControlController(), animated: true)
         }
-        
-        leftButton.button.addTarget(self, action: #selector(MainController.leftButtonPress), for: .touchDown)
-        rightButton.button.addTarget(self, action: #selector(MainController.rightButtonPress), for: .touchDown)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        switch client.connect(timeout: 3) {
-        case .success:
-            print("Connection successful 🎉")
-        case .failure(let error):
-            print("Connectioned failed 💩")
-            print(error)
-        }
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        client.close()
     }
     
     fileprivate var captureModeButton: Button {
         return mainXmlView!.elementForId("captureModeButton")!.asButton!
     }
-    
     fileprivate var controlModeButton: Button {
         return mainXmlView!.elementForId("controlModeButton")!.asButton!
-    }
-    fileprivate var leftButton: Button {
-        return mainXmlView!.elementForId("leftButton")!.asButton!
-    }
-    fileprivate var rightButton: Button {
-        return mainXmlView!.elementForId("rightButton")!.asButton!
     }
 
 }
