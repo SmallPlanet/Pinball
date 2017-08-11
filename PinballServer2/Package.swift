6b8a606a642c3b2841d4f0c064a350ddd7bacaae
Rocco Bowling <rocco@chimerasw.com>
2017-08-11 10:49:02 -0400

    removing old pinball server project

--- PinballServer2/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "PinballServer",
-    dependencies: [
-        .Package(url: "https://github.com/jatoben/CommandLine.git", Version(3, 0, 0, prereleaseIdentifiers: ["pre"], buildMetadataIdentifier: "1")),
-    ]
-)
--- PinballServer2/README.md
+++
@@ -1,3 +0,0 @@
-# TEST
-
-A description of this package.
