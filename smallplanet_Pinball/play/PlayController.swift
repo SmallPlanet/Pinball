8eac654919b523eac49b09eb21bb86c7938c415e
Rocco Bowling <rocco@chimerasw.com>
2017-08-30 15:04:00 -0400

    WIP: testing new non-masked training set

--- smallplanet_Pinball/play/PlayController.swift
+++ smallplanet_Pinball/play/PlayController.swift
@@ -190,7 +190,7 @@ class PlayController: PlanetViewController, CameraCaptureHelperDelegate, Pinball
             
             DispatchQueue.global(qos: .background).async {
                 do {
-                    let imagesPath = String(bundlePath: "bundle://Assets/play/validate_nascar2/")
+                    let imagesPath = String(bundlePath: "bundle://Assets/play/validate_nascar3/")
                     let directoryContents = try FileManager.default.contentsOfDirectory(at: URL(fileURLWithPath:imagesPath), includingPropertiesForKeys: nil, options: [])
                     
                     var allFiles = directoryContents.filter{ $0.pathExtension == "jpg" }
