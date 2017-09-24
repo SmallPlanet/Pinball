23055f0be5c587aa1deba212a5486fd941fe0ee6
Quinn McHenry <quinn@jqm.us>
2017-09-24 08:34:05 -0400

    Add threshold accuracies and left/right at 0.5 threshold

--- validation/Sources/validation/main.swift
+++ validation/Sources/validation/main.swift
@@ -7,12 +7,15 @@ class Validator {
 
     let imagesPath: String
     let model: MLModel
+    let thresholds: [Double]
     
     var processedCount = 0
-    var currentValues = (left: 0, right: 0)
+    var currentValues = (left: false, right: false)
     var totalFiles = 0
     var correct = 0
     var incorrect = 0
+    var thresholdCorrect = [Int: Int]()
+    var categoryCorrect = (left: 0, right: 0)
 
     var percentCorrect: Double {
         return Double(correct)/Double(correct+incorrect)*100.0
@@ -32,12 +35,20 @@ class Validator {
             let leftResult = results.filter{ $0.identifier == "left" }.first
             let rightResult = results.filter{ $0.identifier == "right" }.first
             
-            let confidence = (left: leftResult?.confidence ?? 0, right: rightResult?.confidence ?? 0)
-            let model_output = (left: confidence.left > 0.5 ? 1 : 0, right: confidence.right > 0.5 ? 1 : 0)
+            let confidence = (left: Double(leftResult?.confidence ?? 0), right: Double(rightResult?.confidence ?? 0))
+            let model_output = (left: confidence.left > 0.5, right: confidence.right > 0.5)
+            self.categoryCorrect.left += model_output.left == self.currentValues.left ? 1 : 0
+            self.categoryCorrect.right += model_output.right == self.currentValues.right ? 1 : 0
+
+            self.thresholds.enumerated().forEach { index, threshold in
+                let prediction = (left: confidence.left > threshold, right: confidence.right > threshold)
+                if prediction == self.currentValues {
+                    self.thresholdCorrect[index] = (self.thresholdCorrect[index] ?? 0) + 1
+                }
+            }
             
             self.processedCount += 1
 
-//            print(confidence, model_output, self.currentValues)
             if model_output == self.currentValues {
                 self.correct += 1
             } else {
@@ -59,7 +70,7 @@ class Validator {
                 do {
                     request.imageCropAndScaleOption = .scaleFill
                     let components = file.lastPathComponent.split(separator: "_", maxSplits: 8, omittingEmptySubsequences: true)
-                    currentValues = (left: Int(components[1]) ?? 0, right: Int(components[2]) ?? 0)
+                    currentValues = (left: Int(components[1]) ?? 0 == 1, right: Int(components[2]) ?? 0 == 1)
                     try handler.perform([request])
                 } catch {
                     print(error)
@@ -68,11 +79,23 @@ class Validator {
         }
         
         print("\nCorrect: \(correct)  Incorrect: \(incorrect)  \(percentCorrect)%")
+        print("\nThreshold accuracies:")
+        thresholds.enumerated().forEach { (index, threshold) in
+            print(String(format: "%8f   %0.4f", threshold, Double(thresholdCorrect[index] ?? 0)/Double(processedCount)))
+        }
+        print(String(format: "%0.4f left    %0.4f right", Double(categoryCorrect.left)/Double(processedCount), Double(categoryCorrect.right)/Double(processedCount)))
+
     }
     
-    init(imagesPath: String, model: MLModel) {
+    init(imagesPath: String, model: MLModel, thresholds: [Double] = []) {
         self.imagesPath = imagesPath
         self.model = model
+        if thresholds.isEmpty {
+            self.thresholds = [0.000001, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+        } else {
+            self.thresholds = thresholds
+        }
+        (0..<self.thresholds.count).forEach { thresholdCorrect[$0] = 0 }
     }
     
 }
@@ -80,8 +103,9 @@ class Validator {
 
 
 
+
 if #available(OSX 10.13, *) {
-    let v = Validator(imagesPath: "/Users/quinnmchenry/Development/PinballML/smallplanet_Pinball/Validation/Images/validate_tng", model: pinball_tng_15a().model)
+    let v = Validator(imagesPath: "/Users/quinnmchenry/Development/PinballML/smallplanet_Pinball/Validation/Images/validate_tng", model: tng_alpha_16h().model)
     v.process()
 } else {
     print("Must be running macOS 10.13 or higher")
