6b0ae83bf42a003bdc23c5e15e0b57d2597db3df
Rocco Bowling <rjbowli@ServerML.attlocal.net>
2017-10-16 09:58:41 -0400

    WIP: server RL script

---
+++ ServerML/model.py
@@ -0,0 +1,32 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.optimizers import SGD
+from keras.layers.normalization import BatchNormalization
+import images
+
+def cnn_model():
+
+    model = Sequential()
+    model.add(Conv2D(2, kernel_size=3, input_shape=(images.IMG_SIZE[1], images.IMG_SIZE[0], images.IMG_SIZE[2])))
+    model.add(Activation('elu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    
+    model.add(Conv2D(2, kernel_size=3))
+    model.add(Activation('elu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Flatten())
+    model.add(Dense(768))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(images.NUM_CLASSES))
+    model.add(Activation('sigmoid'))
+    
+    return model
+
+
