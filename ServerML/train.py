125bb9ed0501f558feaac6c856ae89ad2ad89be4
Rocco Bowling <rjbowli@RLServer.attlocal.net>
2017-11-06 11:26:07 -0500

    new WeightedLR will adjust the learning rate on a per batch basis based on the average reward of the memories in that batch

--- ServerML/train.py
+++ ServerML/train.py
@@ -5,6 +5,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import SGD
 from keras import backend as K
 from clr_callback import CyclicLR
+from weighted_learning import WeightedLR
 import model
 import images
 import numpy as np
@@ -96,34 +97,41 @@ def Learn():
     print("Loadinf long term memories...")
     train_imgs = images.generate_image_array(train_path, train_max_size)
     train_labels = []
+    train_weights = []
     
-    images.load_images(train_imgs, train_labels, train_path, train_max_size)
+    images.load_images(train_imgs, train_labels, train_weights, train_path, train_max_size)
     
     print("Load permanent memories...")
     permanent_imgs = images.generate_image_array(permanent_path, permanent_max_size)
     permanent_labels = []
+    permanent_weights = []
     
-    images.load_images(permanent_imgs, permanent_labels, permanent_path, permanent_max_size)
+    images.load_images(permanent_imgs, permanent_labels, permanent_weights, permanent_path, permanent_max_size)
     
     print("Load wasted memories...")
     waste_max_size = len(train_imgs)//3
     waste_imgs = images.generate_image_array(waste_path, waste_max_size)
     waste_labels = []
+    waste_weights = []
     
-    images.load_images(waste_imgs, waste_labels, waste_path, waste_max_size)
+    images.load_images(waste_imgs, waste_labels, waste_weights, waste_path, waste_max_size)
     
     if len(train_labels) > 0 and len(waste_labels) > 0:
         total_imgs = np.concatenate((permanent_imgs,train_imgs,waste_imgs), axis=0)
         total_labels = np.concatenate((permanent_labels,train_labels,waste_labels), axis=0)
+        total_weights = np.concatenate((permanent_weights,train_weights,waste_weights), axis=0)
     elif len(train_labels) > 0:
         total_imgs = np.concatenate((permanent_imgs,train_imgs), axis=0)
         total_labels = np.concatenate((permanent_labels,train_labels), axis=0)
+        total_weights = np.concatenate((permanent_weights,train_weights), axis=0)
     elif len(waste_labels) > 0:
         total_imgs = np.concatenate((permanent_imgs,waste_imgs), axis=0)
         total_labels = np.concatenate((permanent_labels,waste_labels), axis=0)
+        total_weights = np.concatenate((permanent_weights,waste_weights), axis=0)
     else:
         total_imgs = permanent_imgs
         total_labels = permanent_labels
+        total_weights = permanent_weights
     
                     
     if len(total_imgs) >= 6:
@@ -147,23 +155,21 @@ def Learn():
             
             if batch_size > len(total_imgs):
                 batch_size = len(total_imgs)
-            
-            # cyclic learning rate
-            clr = CyclicLR(base_lr=0.001, max_lr=0.006,
-                                    step_size=(len(permanent_imgs) // batch_size * 4), mode='exp_range',
-                                    gamma=0.99994)
+                
+            batch_size = 3
+                                                
+            wlr = WeightedLR(total_weights)
             
             model.fit_generator(datagen.flow(total_imgs, total_labels, batch_size=batch_size),
                     steps_per_epoch=len(total_imgs) // batch_size,
                     epochs=epochs,
-                    callbacks=[clr,em])
+                    callbacks=[wlr,em])
                     
             model.fit(total_imgs, total_labels,
                       batch_size=batch_size,
                       epochs=epochs,
-                      shuffle=True,
                       verbose=1,
-                      callbacks=[clr,em],
+                      callbacks=[wlr,em],
                       )
                 
         print("Training finished...")
---
+++ ServerML/weighted_learning.py
@@ -0,0 +1,51 @@
+from __future__ import division
+from keras.callbacks import *
+
+class WeightedLR(Callback):
+    # this learning rate schedular will adjust the learning rate based upon a factor
+    # known in the training material
+
+    def __init__(self, image_weights, base_lr=0.001, max_lr=0.006):
+        super(WeightedLR, self).__init__()
+        self.image_weights = image_weights
+        self.image_idx = 0
+        self.base_lr = base_lr
+        self.max_lr = max_lr
+        self.max_weight = max(image_weights)
+        
+                
+    def on_epoch_begin(self, epoch, logs):
+        # at the start of each epoch, reset our weight index
+        self.image_idx = 0
+        
+    def on_batch_begin(self, batch, logs):
+        # logs.batch = the index of this batch
+        # logs.size = the number of training samples in this batch
+                
+        # for this batch, run over the next logs.size image wieghts and average them
+        # use this weight to inform the learning rate for this batch
+        log_size = logs.get("size")
+        
+        avg = 0
+        for x in range(self.image_idx, self.image_idx + log_size):
+            avg += self.image_weights[x]
+        
+        avg /= log_size
+        
+        normalized_weight = avg / self.max_weight
+        
+        weighted_lr = self.base_lr + (self.max_lr - self.base_lr) * normalized_weight
+        
+        # now we have the average of all weights in this batch, normalize them to
+        # put them in a reasonable range for learning rate
+        K.set_value(self.model.optimizer.lr,  weighted_lr)
+        
+        # advance our image_idx for next time
+        self.image_idx += log_size
+        
+        
+        
+            
+            
+
+  
