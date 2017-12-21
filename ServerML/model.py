45fc555b29b163417f2ce52f34a2a2c5cea589c6
Rocco Bowling <rjbowli@RLServer.attlocal.net>
2017-12-20 23:37:37 -0500

    larger model for more trainable parameters

--- ServerML/model.py
+++ ServerML/model.py
@@ -10,18 +10,18 @@ def cnn_model():
 
     model = Sequential()
     model.add(Conv2D(4, strides=2, kernel_size=3, input_shape=(images.IMG_SIZE[1], images.IMG_SIZE[0], images.IMG_SIZE[2])))
-    model.add(Activation('elu'))
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
     
     model.add(Conv2D(8, kernel_size=3))
-    model.add(Activation('elu'))
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
 
     model.add(Flatten())
-    model.add(Dense(512))
-    model.add(Activation('elu'))
+    model.add(Dense(1024))
+    model.add(Activation('relu'))
     model.add(BatchNormalization())
     model.add(Dropout(0.5))
     model.add(Dense(images.NUM_CLASSES))
