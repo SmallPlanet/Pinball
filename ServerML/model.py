0859f304a2ac7e781b1d67d6ca2e768d06dbb8a1
Rocco Bowling <rjbowli@RLServer.attlocal.net>
2017-12-22 21:13:02 -0500

    new model with 8m parameters, seems to perform much better than previous model

--- ServerML/model.py
+++ ServerML/model.py
@@ -7,25 +7,28 @@ from keras.layers.normalization import BatchNormalization
 import images
 
 def cnn_model():
-
-    model = Sequential()
-    model.add(Conv2D(4, strides=2, kernel_size=3, input_shape=(images.IMG_SIZE[1], images.IMG_SIZE[0], images.IMG_SIZE[2])))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
     
-    model.add(Conv2D(8, kernel_size=3))
+    model = Sequential()
+    model.add(Conv2D(16, strides=2, kernel_size=3, input_shape=(images.IMG_SIZE[1], images.IMG_SIZE[0], images.IMG_SIZE[2])))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
-
+    
+    model.add(Conv2D(32, kernel_size=3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+        
     model.add(Flatten())
-    model.add(Dense(1024))
+    model.add(Dense(2048))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
-    model.add(Dropout(0.5))
     model.add(Dense(images.NUM_CLASSES))
     model.add(Activation('sigmoid'))
+    
+    #print(model.summary())
+    
+    model.compile(loss='binary_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
     
     return model
 
